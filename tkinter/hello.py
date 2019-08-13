61628a1c6092ed4ccea0e6a5fdf3d3056a922d01
gtechzilla <okiomagerald@gmail.com>
2019-08-13 11:17:20 +0300

    commented the code

--- tkinter/hello.py
+++ tkinter/hello.py
@@ -4,16 +4,18 @@ class window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Hello Tkinter")
-        
+        #creating label widget and setting its position using pack method        
         self.label =tk.Label(self,text ="Choose One")
         self.label.pack(fill=tk.BOTH, expand=1,padx=100,pady=30)
 
+        #creating button widget,setting position using pack method,calling function using command
         home_button = tk.Button(self,text="Say Hello",command=self.say_hello)
         home_button.pack(side=tk.LEFT,padx=(20,0),pady=(0,20))
 
         goodbye_button = tk.Button(self,text=("say Goodbye"),command=self.say_goodbye)
         goodbye_button.pack(side=tk.RIGHT,padx=(0,20),pady=(0,20))
 
+    #defing our functions
     def say_hello(self):
         self.label.configure(text="hello World!")
     def say_goodbye(self):
