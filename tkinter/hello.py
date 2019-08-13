cbe64930176dfaaf433af1e09895113969ae6e06
gtechzilla <okiomagerald@gmail.com>
2019-08-13 10:33:20 +0300

    restructured the directories

---
+++ tkinter/hello.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Hello Tkinter")
+        
+        label =tk.Label(self,text ="Hello World!")
+        label.pack(fill=tk.BOTH, expand=1,padx=100,pady=500)
+if __name__ == "__main__":
+    window =window()
+    window.mainloop()
