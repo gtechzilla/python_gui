b934ce06f6a147bba73f7d3e919a8a29537a1ad1
gtechzilla <okiomagerald@gmail.com>
2019-08-13 11:15:13 +0300

    made our window iteractive by adding button widgets and functions for our commands

--- tkinter/hello.py
+++ tkinter/hello.py
@@ -5,8 +5,20 @@ class window(tk.Tk):
         super().__init__()
         self.title("Hello Tkinter")
         
-        label =tk.Label(self,text ="Hello World!")
-        label.pack(fill=tk.BOTH, expand=1,padx=100,pady=500)
+        self.label =tk.Label(self,text ="Choose One")
+        self.label.pack(fill=tk.BOTH, expand=1,padx=100,pady=30)
+
+        home_button = tk.Button(self,text="Say Hello",command=self.say_hello)
+        home_button.pack(side=tk.LEFT,padx=(20,0),pady=(0,20))
+
+        goodbye_button = tk.Button(self,text=("say Goodbye"),command=self.say_goodbye)
+        goodbye_button.pack(side=tk.RIGHT,padx=(0,20),pady=(0,20))
+
+    def say_hello(self):
+        self.label.configure(text="hello World!")
+    def say_goodbye(self):
+        self.label.configure(text="Goodbye! \n (Closing in 2 seconds)")
+        self.after(2000,self.destroy)
 if __name__ == "__main__":
     window =window()
     window.mainloop()
